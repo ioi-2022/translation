f3e48a3c9944176c67ed4a48615996cadc98a5ef
Ashar Fuadi <fushar@gmail.com>
2021-04-18 23:27:06 +0700

    Update configs and assets for APIO 2021

--- trans/context_processors.py
+++ trans/context_processors.py
@@ -2,11 +2,11 @@ from django.conf import settings
 
 def ioi_settings(request):
     return {'settings': {
-        'SITE_TITLE': 'IOI 2019 Translation Home Page',
-        'CONTEST_TITLE': 'IOI 2019',
-        'CONTEST_FULL_TITLE': 'International Olympiad in Informatics 2019',
-        'CONTEST_DATE': 'August 4\u201311th, 2019',
-        'CONTEST_PLACE': 'Baku, Azerbaijan',
+        'SITE_TITLE': 'APIO 2021 Translation Home Page',
+        'CONTEST_TITLE': 'APIO 2021',
+        'CONTEST_FULL_TITLE': 'Asia-Pacific Informatics Olympiad 2021',
+        'CONTEST_DATE': '22 - 23 May 2021',
+        'CONTEST_PLACE': 'Indonesia',
         'PRINT_ENABLED': settings.PRINT_ENABLED,
         'CUSTOM_PRINT_ENABLED': settings.CUSTOM_PRINT_ENABLED,
         'TIME_ZONE': settings.TIME_ZONE,
